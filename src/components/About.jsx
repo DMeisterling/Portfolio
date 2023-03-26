1819192a3a2ddbc150c2bb66a57f02b0f0797903
Dmeisterling <dan-meisterling@t-online.de>
2023-03-26 17:07:57 +0200

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem non
+          eum corrupti quidem, laborum quo nulla animi nobis a nostrum
+          blanditiis. Qui alias omnis culpa nesciunt commodi excepturi,
+          aspernatur beatae.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci,
+          incidunt at in ratione velit distinctio voluptates pariatur harum,
+          labore odit ea amet esse optio deserunt commodi totam! Ea, commodi
+          similique repudiandae necessitatibus dicta natus sed, voluptatum
+          adipisci id vitae magni nam sequi distinctio mollitia. Odit maiores
+          accusantium ipsum enim earum!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
