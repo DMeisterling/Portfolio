87eb2ab4bd0655c1a349d2a31f21000f8fd40ba9
DMeisterling <dan-meisterling@t-online.de>
2023-03-28 23:04:40 +0200

    changes to legal notice page

--- src/components/LegalNotice.jsx
+++ src/components/LegalNotice.jsx
@@ -3,14 +3,41 @@ import React from "react";
 const LegalNotice = () => {
   return (
     <div name="impressum" className="h-screen w-full">
-      <div class="fit mx-auto flex flex-col items-center justify-center h-80 px-4 text-center">
+      <div class="mx-auto flex flex-col items-center justify-center h-80 px-4 text-center">
         <h2 class="text-3xl sm:text-7xl md:text-7xl font-bold border-b-4 border-l-4 border-gray-500 pb-4 px-2">
           Impressum
         </h2>
       </div>
-      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center px-4">
-        <div className="flex flex-col justify-center max-w-lg w-full">
-          <div className="flex justify-center"></div>
+      <div className="mx-auto flex flex-col items-center justify-center h-80 px-4 text-center">
+        <div className="text-xl sm:text-2xl md:text-2xl pb-4 px-2 font-thin">
+          <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+          <br />
+          <p>
+            Daniel Meisterling
+            <br />
+            Marie-Luise-Flei&szlig;er-Weg 11
+            <br />
+            84489 Burghausen
+          </p>
+          <br />
+          <h2 className="border-b pb-2">Kontakt</h2>
+          <p className="pt-2">
+            Telefon: +49 173 8419767
+            <br />
+            E-Mail: dan-meisterling@t-online.de
+          </p>
+
+          <p>
+            Quelle: &nbsp;
+            <a
+              href="https://www.e-recht24.de"
+              className="border-b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://www.e-recht24.de/impressum-generator.html
+            </a>
+          </p>
         </div>
       </div>
     </div>
