8a76f5c4fb6bbf7f662056281b8cd99675ad5a8f
DMeisterling <dan-meisterling@t-online.de>
2023-03-28 23:56:09 +0200

    open github in new tab, change readme

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -48,6 +48,7 @@ const Portfolio = () => {
               />
               <form
                 action="https://github.com/DMeisterling/Portfolio"
+                target="_blank"
                 className="flex items-center justify-center"
               >
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
