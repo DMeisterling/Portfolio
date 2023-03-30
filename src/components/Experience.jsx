5d8f51873910a1bdf9f8e215f69cddc743faf464
DMeisterling <dan-meisterling@t-online.de>
2023-03-30 23:34:27 +0200

    added code to sort experience from highest to lowest

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -100,6 +100,20 @@ const Experience = () => {
     },
   ];
 
+// sort by name
+techs.sort((a, b) => {
+  const nameA = a.experience.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.experience.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return 1;
+  }
+  if (nameA > nameB) {
+    return -1;
+  }
+  // names must be equal
+  return 0;
+});
+
   return (
     <div
       name="erfahrung"
