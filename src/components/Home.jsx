cf1e9e53bd65209959f7f3d775c988c2420be279
DMeisterling <dan-meisterling@t-online.de>
2023-03-30 19:31:31 +0200

    highlighting home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="pb-8 pt-4 sm:pt-12 w-full dark:bg-gradient-to-b dark:from-gray-900 dark:via-dark-emerald dark:to-gray-900 bg-gradient-to-b  from-slate-200 via-slate-300 to-slate-200"
+      className="pb-8 pt-10 sm:pt-20 w-full dark:bg-gradient-to-b dark:from-gray-900 dark:via-dark-emerald dark:to-gray-900 bg-gradient-to-b  from-slate-200 via-slate-300 to-slate-200"
     >
-      <div class="mx-auto mb-auto flex flex-col items-center justify-center h-40 px-4 text-center">
+      <div class="mx-auto mb-auto flex flex-col items-center justify-center h-40 px-4 text-center pb-4">
         <h2 class="text-3xl sm:text-7xl md:text-7xl font-bold pb-2 px-2">
           Daniel Meisterling
         </h2>
@@ -21,15 +21,15 @@ const Home = () => {
         <div className="flex flex-col justify-center max-w-lg w-full">
           <ul className="dark:text-gray-200 text-black p-4 list-disc">
             <li className="pt-4">
-              Full-Stack Software-Developement mit JavaScript, Angular und
-              TypeScript
+              <strong>Full-Stack Software-Developement</strong> mit <strong className="underline">JavaScript</strong>, <strong className="underline">Angular </strong>und
+              <strong className="underline"> TypeScript</strong>
             </li>
-            <li className="pt-3">API-Aufrufe für REST-APIs via Postman</li>
+            <li className="pt-3">API-Aufrufe für REST-APIs via <strong className="underline">Postman </strong></li>
             <li className="pt-3">
-              Datenbank-Queries für u.A. MSSQL und PostgreSQL
+              Datenbank-Queries für u.A. <strong className="underline">MSSQL </strong>und <strong className="underline">PostgreSQL </strong>
             </li>
             <li className="pt-3">
-              Testing und Dokumentation von neuen Funktionen via PHPUnit
+              Testing und Dokumentation von neuen Funktionen via <strong className="underline">PHPUnit </strong>
             </li>
             <li className="pt-3">
               Versionierungskontrolle mit zyklischen Releases
@@ -38,10 +38,10 @@ const Home = () => {
               Pluginaktualisierung für eine Authentifizierung mit LDAP
             </li>
             <li className="pt-3">
-              Agile Coding mit Scrum-Methoden und Sprints
+              Agile Coding mit <strong className="underline">Scrum-</strong>Methoden und Sprints
             </li>
             <li className="pt-3">
-              Code- und Designanpassungen in kundenorientierten Lösungsansätzen
+              Code- und Designanpassungen in <strong className="underline">kundenorientierten </strong>Lösungsansätzen
             </li>
           </ul>
           <div className="flex justify-center">
