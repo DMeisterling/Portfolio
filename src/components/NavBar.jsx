17bf5d519405de3498db3a339db1fb1eeda04eb7
DMeisterling <dan-meisterling@t-online.de>
2023-03-30 20:54:08 +0200

    center the mobile burger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -80,7 +80,7 @@ const NavBar = ({ darkMode, setDarkMode }) => {
 
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        className="cursor-pointer z-10 text-gray-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
