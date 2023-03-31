0fdf6b31b783e56d2c26b0df288763557534847b
DMeisterling <dan-meisterling@t-online.de>
2023-03-31 17:41:37 +0200

    fixed legal notice component on mobile landscape view

--- src/components/LegalNotice.jsx
+++ src/components/LegalNotice.jsx
@@ -4,15 +4,15 @@ const LegalNotice = () => {
   return (
     <div
       name="impressum"
-      className="h-screen w-full dark:bg-gradient-to-b dark:from-gray-900 dark:to-dark-emerald bg-gradient-to-b  from-slate-200 via-slate-300 to-slate-200"
+      className="min-h-screen h-full vh-100 w-full dark:bg-gradient-to-b dark:from-gray-900 dark:to-dark-emerald bg-gradient-to-b  from-slate-200 via-slate-300 to-slate-200 pt-14"
     >
-      <div class="mx-auto flex flex-col items-center justify-center h-80 px-4 text-center">
-        <h2 class="text-3xl sm:text-7xl md:text-7xl font-bold border-b-4 border-l-4 border-gray-500 pb-4 px-2">
+      <div className="mx-auto flex flex-col items-center justify-center px-4 text-center">
+        <h2 className="text-3xl sm:text-7xl md:text-7xl font-bold border-b-4 border-gray-500 pb-4 px-2">
           Impressum
         </h2>
       </div>
-      <div className="mx-auto flex flex-col items-center justify-center h-80 px-4">
-        <div className="text-xl sm:text-2xl md:text-2xl pb-4 px-2 font">
+      <div className="mx-auto flex flex-col items-center justify-center px-4">
+        <div className="pt-8 text-xl sm:text-2xl md:text-2xl pb-4 px-2">
           <h2 className="border-b-4 border-gray-500 pb-2 font-bold">
             Angaben gem&auml;&szlig; &sect; 5 TMG
           </h2>
