6b875e1e9335a4634447980225a890b41df8a19a
DMeisterling <dan-meisterling@t-online.de>
2023-04-01 23:20:11 +0200

    changed padding from about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       className="w-full dark:bg-gradient-to-b dark:from-gray-900 dark:via-dark-emerald dark:to-gray-900 bg-gradient-to-b  from-slate-200 via-slate-300 to-slate-200"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pt-4">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Über mich
           </p>
