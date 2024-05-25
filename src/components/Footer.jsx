442ef161b61798450063e67fd23023e9d4a0e5f9
DMeisterling <dan-meisterling@t-online.de>
2024-05-25 16:05:48 +0200

    Update Footer Year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <section className="min-h-fit flex flex-col justify-start items-center py-16 px-5 text-center dark:bg-gradient-to-b dark:from-gray-900 dark:to-dark-emerald bg-gradient-to-b  from-slate-200 via-slate-400 to-slate-500">
       <p className="max-w-xl font-bold text-gray-900 dark:text-gray-500 mb-10">
-        © 2023 Daniel Meisterling
+        © 2024 Daniel Meisterling
       </p>
       <Link to="/Impressum" title="Impressum">
         Impressum
